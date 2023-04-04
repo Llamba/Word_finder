b6131bfa8e82b3dfefb1baade2ae7c394bab9dc5
Llamba <marvelcalvero@gmailc.om>
2023-04-03 21:41:50 -0400

    added hyperlinks on words

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,13 @@
 class SearchController < ApplicationController
     def index
-        query = params[:q]
+        query = params[:query]
         @min_word_length = params[:min_word_length]
         @max_word_length = params[:max_word_length]
         @starts_with = params[:starts_with]
         @ends_with = params[:ends_with]
         @dont_include = params[:dont_include]
-        @results = search_words(query, @min_word_length, @max_word_length, @starts_with, @ends_with, @dont_include)
+        @word_typed_only = params[:word_typed_only]
+        @results = search_words(query, @min_word_length, @max_word_length, @starts_with, @ends_with, @dont_include, @word_typed_only)
       end
     def search
         @query = params[:query]
@@ -15,66 +16,81 @@ class SearchController < ApplicationController
         @starts_with = params[:starts_with]
         @ends_with = params[:ends_with]
         @dont_include = params[:dont_include]
-        @results = search_words(@query, @min_word_length, @max_word_length, @starts_with, @ends_with, @dont_include)
+        @word_typed_only = params[:word_typed_only]
+        @results = search_words(@query, @min_word_length, @max_word_length, @starts_with, @ends_with, @dont_include, @word_typed_only)
       end
       private
-    
-      def search_words(search, min_word_length, max_word_length, starts_with, ends_with, dont_include)
+
+      def search_words(search, min_word_length, max_word_length, starts_with, ends_with, dont_include, word_typed_only)
         File.open("movies.txt", "r+") do |text|
+          data = text.readlines.map { |line| line.downcase.chomp.split(/\s+/) }
           @results = []
+
+          if search && search.split.all? { |term| data.flatten.include?(term) }
+            if word_typed_only == "true"
+              @results << search
+              return @results
+            end
+          end
+
+
           if min_word_length == ""
             min_word_length = nil
           end
+
           if max_word_length == ""
             max_word_length = nil
           end
+
           if starts_with == ""
             starts_with = nil
           end
+
           if ends_with == ""
             ends_with = nil
           end
+
           if dont_include.blank?
             dont_include = nil
           else
             dont_include = dont_include.split(',')
           end
+
           min_word_length = min_word_length.to_i if min_word_length
           max_word_length = max_word_length.to_i if max_word_length
 
-          data = text.readlines.map { |line| line.downcase.chomp.split(/\s+/) }
           parsed = search.split if search
-      
+
           data.each do |section|
             if parsed
                 if parsed.all? { |term| section.any? { |attr| attr.include?(term)}}
                 add_word = true
                 word = section[0]
-        
+
                 if max_word_length
                     if word.size > max_word_length
                     add_word = false
                     end
                 end
-        
+
                 if min_word_length
                     if word.size < min_word_length
                     add_word = false
                     end
                 end
-        
+
                 if ends_with
                     unless word.end_with?(ends_with)
                     add_word = false
                     end
                 end
-        
+
                 if starts_with
                     unless word.start_with?(starts_with)
                     add_word = false
                     end
                 end
-        
+
                 if dont_include
                     dont_include.each do |current|
                     if word.include?(current)
@@ -83,7 +99,7 @@ class SearchController < ApplicationController
                     end
                     end
                 end
-        
+
                 if add_word
                     @results << word
                 end
@@ -93,4 +109,4 @@ class SearchController < ApplicationController
         end
         @results
       end
-    end
+      end
