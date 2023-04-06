28383ca2e3ab895ebbb3cfc7d0ed0881e3801c7a
Llamba <marvelcalvero@gmailc.om>
2023-04-05 22:39:26 -0400

    minor optimization to search_controller.rb, as well as minor changes to the way the words are read for the search function to make it quicker.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,22 +1,14 @@
 class SearchController < ApplicationController
     def index
         query = params[:query]
-        @min_word_length = params[:min_word_length].presence
-        @max_word_length = params[:max_word_length].presence
-        @starts_with = params[:starts_with].presence
-        @ends_with = params[:ends_with].presence
-        @dont_include = params[:dont_include].presence
-        @word_typed_only = params[:word_typed_only].presence
+        @min_word_length, @max_word_length, @starts_with, @ends_with, @dont_include, @word_typed_only =
+          params.transform_values(&:presence).values_at(:min_word_length, :max_word_length, :starts_with, :ends_with, :dont_include, :word_typed_only)
         @results = SearchWords.search(query, @min_word_length, @max_word_length, @starts_with, @ends_with, @dont_include, @word_typed_only)
     end
     def search
         @query = params[:query]
-        @min_word_length = params[:min_word_length].presence
-        @max_word_length = params[:max_word_length].presence
-        @starts_with = params[:starts_with].presence
-        @ends_with = params[:ends_with].presence
-        @dont_include = params[:dont_include].presence
-        @word_typed_only = params[:word_typed_only].presence
+        @min_word_length, @max_word_length, @starts_with, @ends_with, @dont_include, @word_typed_only =
+          params.transform_values(&:presence).values_at(:min_word_length, :max_word_length, :starts_with, :ends_with, :dont_include, :word_typed_only)
         @results = SearchWords.search(@query, @min_word_length, @max_word_length, @starts_with, @ends_with, @dont_include, @word_typed_only)
     end
 end
